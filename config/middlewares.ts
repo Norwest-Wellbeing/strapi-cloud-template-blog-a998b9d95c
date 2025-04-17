0990a918070d486490f458e60ffe1f588fc29a37
Ciaran Smith <github@smithey.com.au>
2025-04-18 01:08:00 +1000

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,23 +1,23 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  'global::deepPopulate'
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  "global::deepPopulate",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'script-src': ['https://cdn.ckeditor.com'],
-          'connect-src': ['https://proxy-event.ckeditor.com']
+          "script-src": ["https://cdn.ckeditor.com"],
+          "connect-src": ["https://proxy-event.ckeditor.com"],
         },
       },
     },
