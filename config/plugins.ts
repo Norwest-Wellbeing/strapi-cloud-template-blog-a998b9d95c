fc8940ce59ac5902ba6f0547c6d0d002a18b6a90
Ciaran Smith <github@smithey.com.au>
2025-04-17 22:59:07 +1000

    Amazon SES

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "amazon-ses",
+      providerOptions: {
+        key: env("AWS_SES_KEY"),
+        secret: env("AWS_SES_SECRET"),
+        amazon: env("AWS_SES_URL"),
+      },
+      settings: {
+        defaultFrom: env("AWS_SES_FROM"),
+        defaultReplyTo: env("AWS_SES_REPLYTO"),
+      },
+    },
+  },
+});
