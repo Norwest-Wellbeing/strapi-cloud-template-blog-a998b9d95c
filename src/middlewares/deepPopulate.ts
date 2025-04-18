7520e7c8ccf91487bf4e390782e946ecde1ba72a
Paul Smith <paul@datanetgroup.com.au>
2025-04-18 20:27:32 +1000

    Update deepPopulate.ts

--- src/middlewares/deepPopulate.ts
+++ src/middlewares/deepPopulate.ts
@@ -3,10 +3,8 @@
  */
 
 import type { Core, Schema } from '@strapi/strapi';
-import { UID } from '@strapi/types';
 import { contentTypes } from '@strapi/utils';
 import pluralize from 'pluralize';
-
 
 interface Options {
   /**
@@ -19,14 +17,22 @@ const { CREATED_BY_ATTRIBUTE, UPDATED_BY_ATTRIBUTE } = contentTypes.constants;
 
 const extractPathSegment = (url: string) => url.match(/\/([^/?]+)(?:\?|$)/)?.[1] || '';
 
-const getDeepPopulate = (uid: Schema.UID, opts: Options = {}) => {
+const getDeepPopulate = (uid: string, opts: Options = {}) => {
   const model = strapi.getModel(uid);
-  const attributes = Object.entries(model.attributes);
+  const attributes = Object.entries(model.attributes) as [
+    string,
+    {
+      type: string;
+      relation?: string;
+      component?: string;
+      components?: string[];
+    }
+  ];
 
   return attributes.reduce((acc: any, [attributeName, attribute]) => {
     switch (attribute.type) {
       case 'relation': {
-        const isMorphRelation = attribute.relation.toLowerCase().startsWith('morph');
+        const isMorphRelation = attribute.relation?.toLowerCase().startsWith('morph');
         if (isMorphRelation) {
           break;
         }
@@ -35,24 +41,20 @@ const getDeepPopulate = (uid: Schema.UID, opts: Options = {}) => {
         const isVisible = contentTypes.isVisibleAttribute(model, attributeName);
         const isCreatorField = [CREATED_BY_ATTRIBUTE, UPDATED_BY_ATTRIBUTE].includes(attributeName);
 
-        if (isVisible) {
-          if (attributeName === 'testimonials') {
-            acc[attributeName] = { populate: "user.image" };
-          } else {
-            acc[attributeName] = { populate: "*" };
-          }
+        if (isVisible || isCreatorField) {
+          acc[attributeName] = { populate: '*' };
         }
 
         break;
       }
 
       case 'media': {
-        acc[attributeName] = { populate: "*" };
+        acc[attributeName] = { populate: '*' };
         break;
       }
 
       case 'component': {
-        const populate = getDeepPopulate(attribute.component, opts);
+        const populate = getDeepPopulate(attribute.component!, opts);
         acc[attributeName] = { populate };
         break;
       }
@@ -60,7 +62,7 @@ const getDeepPopulate = (uid: Schema.UID, opts: Options = {}) => {
       case 'dynamiczone': {
         // Use fragments to populate the dynamic zone components
         const populatedComponents = (attribute.components || []).reduce(
-          (acc: any, componentUID: Schema.UID) => {
+          (acc: any, componentUID: string) => {
             acc[componentUID] = { populate: getDeepPopulate(componentUID, opts) };
 
             return acc;
